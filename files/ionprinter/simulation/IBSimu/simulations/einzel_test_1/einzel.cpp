63b0dd4f88d47daec0042486dd63495ef26aac11
BasedOnTechnology <therobotist@gmail.com>
2018-06-18 22:00:45 -0400

    Functioning ion beam simulation!

--- files/ionprinter/simulation/IBSimu/simulations/einzel_test_1/einzel.cpp
+++ files/ionprinter/simulation/IBSimu/simulations/einzel_test_1/einzel.cpp
@@ -8,53 +8,80 @@
 #include "ibsimu.hpp"
 #include "error.hpp"
 
+#define EINZEL_EXTRA_R 0.01
+#define SIM_EXTRA_R 0.025
+#define BEAM_Y_TOP 0.055
+#define BEAM_Y_BOTTOM 0.045 
 
-bool solid1( double x, double y, double z )
+bool einzel_1( double x, double y, double z )
 {
-    return( x <= 0.02 && y >= 0.018 );
+    return( x <= 0.05 && (y >= (BEAM_Y_TOP+EINZEL_EXTRA_R) || y <= (BEAM_Y_BOTTOM-EINZEL_EXTRA_R) ));
 }
 
 
-bool solid2( double x, double y, double z )
+bool einzel_2( double x, double y, double z )
 {
-    return( x >= 0.03 && x <= 0.04 && y >= 0.02 );
+    return( x >= 0.06 && x <= 0.11 && (y >= (BEAM_Y_TOP+EINZEL_EXTRA_R) || y <= (BEAM_Y_BOTTOM-EINZEL_EXTRA_R) ));
 }
 
-
-bool solid3( double x, double y, double z )
+bool einzel_3( double x, double y, double z )
 {
-    return( x >= 0.06 && y >= 0.03 && y >= 0.07 - 0.5*x );
+    return( x >= 0.12 && x <= 0.2 && (y >= (BEAM_Y_TOP+EINZEL_EXTRA_R) || y <= (BEAM_Y_BOTTOM-EINZEL_EXTRA_R) ));
 }
-
 
 void simu( void )
 {
-    Geometry geom( MODE_2D, Int3D(241,101,1), Vec3D(0,0,0), 0.0005 );
+    double sim_res = 0.0005; //m
+    double sim_width = BEAM_Y_TOP+EINZEL_EXTRA_R+SIM_EXTRA_R; //m
+    double sim_length = 0.6; //m
+    Geometry geom( MODE_2D, Int3D(sim_length/sim_res,sim_width/sim_res,1), Vec3D(0,0,0), sim_res);
 
-    Solid *s1 = new FuncSolid( solid1 );
+    Solid *s1 = new FuncSolid( einzel_1 );
     geom.set_solid( 7, s1 );
-    Solid *s2 = new FuncSolid( solid2 );
+    Solid *s2 = new FuncSolid( einzel_2 );
     geom.set_solid( 8, s2 );
-    Solid *s3 = new FuncSolid( solid3 );
+    Solid *s3 = new FuncSolid( einzel_3 );
     geom.set_solid( 9, s3 );
     
-    geom.set_boundary( 1, Bound(BOUND_DIRICHLET,  0.0e3) );
-    geom.set_boundary( 2, Bound(BOUND_DIRICHLET,  0.0e3) );
+    geom.set_boundary( 1, Bound(BOUND_DIRICHLET,  0.0) );
+    geom.set_boundary( 2, Bound(BOUND_DIRICHLET,  0.0) );
     geom.set_boundary( 3, Bound(BOUND_NEUMANN,     0.0  ) );
     geom.set_boundary( 4, Bound(BOUND_NEUMANN,     0.0  ) );
-    geom.set_boundary( 7, Bound(BOUND_DIRICHLET,  -3.0e3) );
-    geom.set_boundary( 8, Bound(BOUND_DIRICHLET, -14.0e3) );
-    geom.set_boundary( 9, Bound(BOUND_DIRICHLET,  -1.0e3) );
+    geom.set_boundary( 7, Bound(BOUND_DIRICHLET,  0.0) );
+    geom.set_boundary( 8, Bound(BOUND_DIRICHLET,  -100.0) );
+    geom.set_boundary( 9, Bound(BOUND_DIRICHLET,  0.0) );
     geom.build_mesh();
 
     EpotField epot( geom );
     MeshScalarField scharge( geom );
-    MeshVectorField bfield;
     EpotEfield efield( epot );
     field_extrpl_e efldextrpl[6] = { FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE, 
 				     FIELD_SYMMETRIC_POTENTIAL, FIELD_EXTRAPOLATE,
 				     FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE };
     efield.set_extrapolation( efldextrpl );
+    double Bz = 0.0;
+    const bool fout[3] = {false, false, true};
+    MeshVectorField extbfield(MODE_2D, fout, Int3D(100,100,1),Vec3D(0.00,0.00,0), 0.0005);
+    for( uint32_t i = 0; i < extbfield.size(0); i++ ) {
+        //double x = extbfield.origo(0)+i*bfield.h();
+        for( uint32_t j = 0; j < extbfield.size(1); j++ ) {
+            //double y = extbfield.origo(1)+j*bfield.h();
+            for ( uint32_t k = 0; k < extbfield.size(2); k++ ) {
+                //double z = extbfield.origo(2)+k*bfield.h();
+		
+
+		if(i < 50){
+	                Bz = 0.0; //0.5 T
+		}
+		else{
+	                Bz = -0.0; //-0.5 T
+		}
+                extbfield.set( i, j, k, Vec3D( 0, 0, Bz ) );
+            }
+        }
+    }
+
+
 
     EpotBiCGSTABSolver solver( geom );
 
@@ -66,18 +93,28 @@ void simu( void )
 	solver.solve( epot, scharge );
 	efield.recalculate();
 	pdb.clear();
-	pdb.add_2d_beam_with_energy( 1000, 5000000, 1.0, 1.0, 
-				     5000, 0.0, 0.0, 
-				     0.0, 0.0, 
-				     0.0, 0.0005 );
-	pdb.iterate_trajectories( scharge, efield, bfield );
+	pdb.add_2d_beam_with_energy( 1000, //beam traces 
+					 5000000, //beam current, A/m^2
+					 1.0, //particle charge, q
+					 29.0, //mass in AMU of the particles 
+					 50, //Energy of the particles, EV 
+					 0.0,
+					 0.0, 
+					 0.0,
+					 BEAM_Y_BOTTOM, 
+					 0.0, 
+					 BEAM_Y_TOP );
+	pdb.set_max_steps(15);
+	pdb.iterate_trajectories( scharge, efield, extbfield );
     }
 
     GeomPlotter geomplotter( geom );
-    geomplotter.set_size( 750, 750 );
+    geomplotter.set_size( 1500, 300 );
     geomplotter.set_epot( &epot );
+    geomplotter.set_bfield( &extbfield );
     geomplotter.set_particle_database( &pdb );
-    geomplotter.plot_png( "plot1.png" );
+    geomplotter.set_fieldgraph_plot(FIELD_BFIELD_Z);
+    geomplotter.plot_svg( "plot1.svg" );
 }
 
 
--- project_description.md
+++ project_description.md
@@ -25,7 +25,7 @@ Leak.
 Did a huge amount of reading (mainly Goebels et al and nasa ion thruster datasheets) over florida vacation, and I believe an inductively coupled plasma might be the way to go now. Currently, I don't really know what the charge on each droplet of aluminum is - how many atoms the vapour is composed of, etc. With a powerful enough ICP coil, I'll get full ionization for every single atom, making the math for deflection easier. With a mass flow rate of about 0.01g/s (easily achivable with the current 300w filament system), the complete ionization energy is somewhere around 500 watts. I've found an efficiency figure of 40% from a few ICP systems, so basically I need a 1kw RF supply. I think I'm going to go with an "induction heater"-style resonant ZVS royer driver - since everything's fully shielded, I don't think I'm going to have to worry about the frequency that much; and using a resonant system means I can skip antenna matching, which was an issue for me in the past. Good for testing, anyhow.
 
 Mass flow rate: $$2.232×10^20 atoms/s, 0.01g/s$$ - about 40 grams an hour
-2.407678×10^-18 j/atom - 15 ev, roughly $$1/2((29 amu)((10000m/s)^2))$$
+2.407678×10^-18 j/atom - 15 ev, roughly $$1/2((29 amu)((10000m/s)^2))$$... roughly 500 watts of beam power.
 
 
 Regarding the ICP ionization, the thermal velocity of the gaseous aluminum will be around 1.7 km/s, meaning at 250khz the atoms'll move around 7 mm before being ionized - if the coil isn't long enough, the atoms'll be out of the magnetic field by the time they leave.
@@ -152,7 +152,22 @@ Since we need to deposit all the materials in the alloy simultaneously, not sequ
 
 Wire feedthroughs: Buna-N has a TML of around 7; I don't even have to worry about a tiny bit of JB-weld, I don't think.
 
-
 The Bowtie; the graphite that I purchased from mcmaster has an exceptionally low resistivity; around 1.54×10^-5 ohm meters. This means that a 10mm long 4*4mm rod would have a resistance of 0.0385 ohms,
 drawiing 20a at 12v. When the injector holes are drilled, and some extra contact resistance is added, this is almost perfect...though this bowtie would also have an area of 80mm^2; a loss of 400w.
+
+Might have some issues dissipating 500-odd watts of beam power in a cm^3 scale part. Liquid-cooled or cryo-chilled build platform might be required.
+
+Even at a super slow print speed of 0.4 g/h, a 1 oz PCB will be done in 17 hours.
+
+A serious problem: the beam current density is much, much higher than standard particle accelerators. A typical value seems to be 50 A/m^2; with a beam diameter of 0.5 cm, the initial beam current density will
+be nearly 125,000. A beam diameter of 1 cm will yield a current density of 31,800; but the electrostatic optics become problematic at these charge density levels.
+
+So, I either have to decrease my beam power (how?), or figure out how to focus at that power.
+
+I could split the beam into seperate beams, each with a lower energy...
+
+A small dot size is only required for fine details, where the print speed can be lower.
+
+Some simple math: 40g/h of aluminum, which is 15 cubic centimeters (a cube 2.46cm to the side).
+
 
