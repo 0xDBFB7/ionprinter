1f04d6a55c7b414bee5261130edec89acd5a5bd5
BasedOnTechnology <therobotist@gmail.com>
2018-12-16 15:23:05 -0500

    Trying cyl. geom

--- files/ionprinter/simulation/IBSimu/simulations/magnetic_focusing_1/einzel.cpp
+++ files/ionprinter/simulation/IBSimu/simulations/magnetic_focusing_1/einzel.cpp
@@ -14,7 +14,9 @@
 #include "gtkplotter.hpp"
 #endif
 
-#define BEAM_RADIUS 0.01
+#define BEAM_RADIUS 0.01 //m
+#define BEAM_CURRENT 35.75 //A
+#define BEAM_ENERGY 15 //eV
 
 const double Te = 5.0;
 const double Up = 5.0;
@@ -35,18 +37,29 @@ bool solid2( double x, double y, double z )
 
 void simu( int *argc, char ***argv )
 {
-    // Geometry geom( MODE_CYL, Int3D(241,141,1), Vec3D(0,0,0), 0.00005 );
-    // Solid *s1 = new FuncSolid( solid1 );
-    // geom.set_solid( 7, s1 );
-    // Solid *s2 = new FuncSolid( solid2 );
-    // geom.set_solid( 8, s2 );
-    // geom.set_boundary( 1, Bound(BOUND_NEUMANN,    0.0 ) );
-    // geom.set_boundary( 2, Bound(BOUND_DIRICHLET, -12.0e3) );
-    // geom.set_boundary( 3, Bound(BOUND_NEUMANN,    0.0) );
-    // geom.set_boundary( 4, Bound(BOUND_NEUMANN,    0.0) );
-    // geom.set_boundary( 7, Bound(BOUND_DIRICHLET,  0.0)  );
-    // geom.set_boundary( 8, Bound(BOUND_DIRICHLET, -12.0e3) );
-    // geom.build_mesh();
+    double start = -3.0e-3;
+    double h = 0.4e-3;
+    double h = 0.4e-3;
+    double sizereq[3] = { 50.0e-3,
+                          50.0e-3,
+                          125.0e-3-start };
+    Int3D meshsize( (int)floor(sizereq[0]/h)+1,
+                  (int)floor(sizereq[1]/h)+1,
+                  (int)floor(sizereq[2]/h)+1 );
+    Vec3D origo( -25.0e-3, -25.0e-3, start );
+
+    Geometry geom( MODE_3D, meshsize, origo, h );
+    Solid *s1 = new FuncSolid( solid1 );
+    geom.set_solid( 7, s1 );
+    Solid *s2 = new FuncSolid( solid2 );
+    geom.set_solid( 8, s2 );
+    geom.set_boundary( 1, Bound(BOUND_NEUMANN,    0.0 ) );
+    geom.set_boundary( 2, Bound(BOUND_DIRICHLET, -12.0e3) );
+    geom.set_boundary( 3, Bound(BOUND_NEUMANN,    0.0) );
+    geom.set_boundary( 4, Bound(BOUND_NEUMANN,    0.0) );
+    geom.set_boundary( 7, Bound(BOUND_DIRICHLET,  0.0)  );
+    geom.set_boundary( 8, Bound(BOUND_DIRICHLET, -12.0e3) );
+    geom.build_mesh();
 
     EpotBiCGSTABSolver solver( geom );
     InitialPlasma initp( AXIS_X, 0.55e-3 );
@@ -61,7 +74,7 @@ void simu( int *argc, char ***argv )
 				     FIELD_EXTRAPOLATE, FIELD_EXTRAPOLATE };
     efield.set_extrapolation( efldextrpl );
 
-    ParticleDataBaseCyl pdb( geom );
+    ParticleDataBase3D pdb( geom );
     bool pmirror[6] = { false, false, true, false, false, false };
     pdb.set_mirror( pmirror );
 
@@ -76,10 +89,20 @@ void simu( int *argc, char ***argv )
     	efield.recalculate();
 
     	pdb.clear();
-      float beam_area = 2.0*M_PI*BEAM_RADIUS^2;
+      float beam_area = 2.0*M_PI*pow(BEAM_RADIUS,2); //m^2
 
     	pdb.add_cylindrical_beam_with_energy(
-                                            10000,
+                                            10000, //number of particles
+                                            BEAM_CURRENT/beam_area, //beam current density
+                                            1.0, //charge per particle
+                                            29.0, //amu
+                                            15, //eV
+                                            1,//Normal temperature
+                                            1,
+                                            (0,0,0),
+                                            (0,1,0),
+                                            (0,0,1),
+                                            BEAM_RADIUS);
 
     	pdb.iterate_trajectories( scharge, efield, bfield );
     }
