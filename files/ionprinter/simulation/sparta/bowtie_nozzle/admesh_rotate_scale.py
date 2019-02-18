ece464bc2fd26758af237b84d6e4aeccd2fa1099
BasedOnTechnology <therobotist@gmail.com>
2019-02-17 20:37:34 -0500

    Fixed gitignore?

--- files/ionprinter/simulation/sparta/bowtie_nozzle/admesh_rotate_scale.py
+++ files/ionprinter/simulation/sparta/bowtie_nozzle/admesh_rotate_scale.py
@@ -1,18 +1,19 @@
 import admesh
 
-# load an STL file
 stl = admesh.Stl('nozzle.stl')
 
+stl.repair()
 stl.scale(0.001)
-stl.translate(0,0,0)
-# stl.repair()
+radius = 0.0003
+stl.translate(0.001-radius,0.001-radius,0.0)
 stl.write_ascii("nozzle-fixed.stl")
 
 # load an STL file
 stl = admesh.Stl('emitting_region.stl')
+stl.repair()
 stl.scale(0.001)
-stl.translate(0.001-0.0001,0.001-0.0001,0.002)
-# stl.repair()
+radius = 0.00007
+stl.translate(0.001-radius,0.001-radius,0.00052)
 stl.write_ascii("emitting_region-fixed.stl")
 #
 # import stl
@@ -21,5 +22,10 @@ stl.write_ascii("emitting_region-fixed.stl")
 # nozzle.save('nozzle-fixed.stl',mode=stl.stl.ASCII)
 #
 # nozzle = stl.mesh.Mesh.from_file('emitting_region.stl')
-# nozzle.transform()
 # nozzle.save('emitting_region.stl',mode=stl.stl.ASCII)
+
+# from pymesh import stl
+# import pymesh
+# m = stl.Stl("nozzle.stl")
+# m.scale(1, 2, 1)
+# m.save_stl("nozzle-fixed.stl", mode=pymesh.MODE_STL_ASCII)
