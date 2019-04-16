9307d5467c76316cbabfd56dbb3d78435ea750e7
BasedOnTechnology <therobotist@gmail.com>
2019-04-15 23:19:41 -0400

    Adding rings

---
+++ files/ionprinter/simulation/DFTBA/AGM_elliptic.py
@@ -0,0 +1,82 @@
+
+MEAN_TOLERANCE = 1.e-10
+
+
+def ag_mean(a,b,tolerance):
+    '''
+    Computes arithmetic-geometric mean of A and B
+    https://scipython.com/book/chapter-2-the-core-python-language-i/questions/the-arithmetic-geometric-mean/
+    '''
+    while abs(a-b) > tolerance:
+        a, b = (a + b) / 2.0, math.sqrt(a * b)
+    return b
+
+def first_kind_elliptic_integral(k):
+    '''
+    Calculate K(k) - okay?
+    https://dlmf.nist.gov/19
+    §19.8(i) Gauss’s Arithmetic-Geometric Mean (AGM)
+    '''
+    return (math.pi/2.0)/ag_mean(1,math.sqrt(1.0-(k**2.0)), MEAN_TOLERANCE)
+
+def compute_big_M(m):
+    '''
+    Prerequisite for E(m)
+    '''
+    return (math.pi/(2.0*first_kind_elliptic_integral(m)*math.sqrt(1-m**2.0)))
+
+def compute_delta(m):
+    '''
+    Prerequisite for E(m)
+    '''
+    return (1.0/math.sqrt(1.0-(m**2.0)))
+
+def differentiate_delta_over_M(m):
+    '''
+    Another prerequisite for E(m)
+    Numerically differentiate the quotient delta/M
+    '''
+    dm=1.0e-6
+    return (((compute_delta(m+dm)/compute_big_M(m+dm))-(compute_delta(m)/compute_big_M(m)))/dm)
+
+def second_kind_elliptic_integral(m):
+    '''
+    Calculate E(m)inem
+
+    E says blow, we increase emittance
+    receive remittance
+    percieve:
+    society's been enhanced
+    oh we got them in a trance
+    science's now advanced
+    in common parlance:
+    dance
+
+    From @ kurzweg2012
+
+    Efficiency could be improved by eliminating duplicate calls of big_M and compute_delta(m)
+
+    Currently slightly broken.
+    '''
+    return (math.pi/2.0)/((1.0/(compute_big_M(m)*compute_delta(m))) + (m*differentiate_delta_over_M(m)))
+
+
+class TestAll(unittest.TestCase):
+
+    def test_ag_mean(self):
+        self.assertAlmostEqual(ag_mean(24,6,1e-7), 13.458, places=3)
+
+    def test_first_kind_elliptic_integral(self):
+        self.assertAlmostEqual(first_kind_elliptic_integral(0.5), 1.685751, places=3)
+        self.assertAlmostEqual(first_kind_elliptic_integral(0), 1.57079632, places=3)
+
+    def test_big_M(self):
+        self.assertAlmostEqual(compute_big_M(0.5), 1.07595, places=3)
+
+    def test_differentiate_delta_over_M(self):
+        self.assertAlmostEqual(differentiate_delta_over_M(0), 1.6, places=3)
+        self.assertAlmostEqual(differentiate_delta_over_M(0.9), 1.6, places=3)
+
+    def test_second_kind_elliptic_integral(self):
+        self.assertAlmostEqual(second_kind_elliptic_integral(0), 1.5707963, places=3)
+        self.assertAlmostEqual(second_kind_elliptic_integral(0.9), 1.15, places=3)
--- files/ionprinter/simulation/DFTBA/dftba.py
+++ files/ionprinter/simulation/DFTBA/dftba.py
@@ -9,99 +9,31 @@ import sys
 import numpy as np
 import unittest
 import time
+from scipy import special
 
 
-MEAN_TOLERANCE = 1.e-10
-
-
-def ag_mean(a,b,tolerance):
+def e_field_ring_of_charge(charge,radial_distance,axial_distance,ring_radius):
     '''
-    Computes arithmetic-geometric mean of A and B
-    https://scipython.com/book/chapter-2-the-core-python-language-i/questions/the-arithmetic-geometric-mean/
+    Equations from @ mandre2007
+    Electric field radial and axial components for the off-axis electric field of
+    a ring of charge.
+    Takes
     '''
-    while abs(a-b) > tolerance:
-        a, b = (a + b) / 2.0, math.sqrt(a * b)
-    return b
+    little_q = ((radial_distance**2.0) + (ring_radius**2.0) + (axial_distance**2.0) + (2.0*radial_distance*ring_radius))
+    mu = (4.0*radial_distance*ring_radius)/little_q
+    s = (charge/(4.0*math.pi*constants.epsilon_0))*(2.0/(math.pi*(little_q**(3.0/2.0))*(1.0-mu)))
 
-def first_kind_elliptic_integral(k):
-    '''
-    Calculate K(k) - okay?
-    https://dlmf.nist.gov/19
-    §19.8(i) Gauss’s Arithmetic-Geometric Mean (AGM)
-    '''
-    return (math.pi/2.0)/ag_mean(1,math.sqrt(1.0-(k**2.0)), MEAN_TOLERANCE)
+    radial_electric_field = s*((1.0/mu)*((2.0*ring_radius*special.ellipk(math.sqrt(mu))*(1-mu))- special.ellipe(math.sqrt(mu))*
+                            (2.0*ring_radius-(mu*(radial_distance+ring_radius)))))
 
-def compute_big_M(m):
-    '''
-    Prerequisite for E(m)
-    '''
-    return (math.pi/(2.0*first_kind_elliptic_integral(m)*math.sqrt(1-m**2.0)))
+    axial_electric_field = s*axial_distance*special.ellipe(math.sqrt(mu))
 
-def compute_delta(m):
-    '''
-    Prerequisite for E(m)
-    '''
-    return (1.0/math.sqrt(1.0-(m**2.0)))
-
-def differentiate_delta_over_M(m):
-    '''
-    Another prerequisite for E(m)
-    Numerically differentiate the quotient delta/M
-    '''
-    dm=1.0e-6
-    return (((compute_delta(m+dm)/compute_big_M(m+dm))-(compute_delta(m)/compute_big_M(m)))/dm)
-
-def second_kind_elliptic_integral(m):
-    '''
-    Calculate E(m)inem
-
-    E says blow, we increase emittance
-    receive remittance
-    percieve:
-    society's been enhanced
-    oh we got them in a trance
-    science's now advanced
-    in common parlance:
-    dance
-
-    From @ kurzweg2012
-
-    Efficiency could be improved by eliminating duplicate calls of big_M and compute_delta(m)
-    '''
-    return (math.pi/2.0)/((1.0/(compute_big_M(m)*compute_delta(m))) + (m*differentiate_delta_over_M(m)))
-
-# def e_field_ring_of_charge(radial_distance,axial_distance,ring_radius,):
-#     '''
-#     Equations from @ mandre2007
-#     Electric field radial and axial components for the off-axis electric field of
-#     a ring of charge.
-#     Takes
-#     '''
-#     little_q_ = radial_distance**2.0
-#     mu_ = 4.0
-
-
+    return radial_electric_field,axial_electric_field
 
 class TestAll(unittest.TestCase):
 
-    def test_ag_mean(self):
-        self.assertAlmostEqual(ag_mean(24,6,1e-7), 13.458, places=3)
-
-    def test_first_kind_elliptic_integral(self):
-        self.assertAlmostEqual(first_kind_elliptic_integral(0.5), 1.685751, places=3)
-        self.assertAlmostEqual(first_kind_elliptic_integral(0), 1.57079632, places=3)
-
-    def test_big_M(self):
-        self.assertAlmostEqual(compute_big_M(0.5), 1.07595, places=3)
-
-    def test_differentiate_delta_over_M(self):
-        self.assertAlmostEqual(differentiate_delta_over_M(0), 1.6, places=3)
-        self.assertAlmostEqual(differentiate_delta_over_M(0.9), 1.6, places=3)
-
-    def test_second_kind_elliptic_integral(self):
-        self.assertAlmostEqual(second_kind_elliptic_integral(0), 1.5707963, places=3)
-        self.assertAlmostEqual(second_kind_elliptic_integral(0.9), 1.15, places=3)
-
+    def test_e_field_ring_of_charge(self):
+        self.assertAlmostEqual(e_field_ring_of_charge(1.0,0,1e10,1e-10),(constants.Coulomb*1)/((1e10)**2.0), places=3)
 
 if __name__ == '__main__':
     unittest.main()
