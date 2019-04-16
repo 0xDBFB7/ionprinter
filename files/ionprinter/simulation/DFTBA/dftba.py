b1221956afb4866134c3295e04e41b16fe2536f1
BasedOnTechnology <therobotist@gmail.com>
2019-04-15 23:30:04 -0400

    Rings working and tested!

--- files/ionprinter/simulation/DFTBA/dftba.py
+++ files/ionprinter/simulation/DFTBA/dftba.py
@@ -23,8 +23,11 @@ def e_field_ring_of_charge(charge,radial_distance,axial_distance,ring_radius):
     mu = (4.0*radial_distance*ring_radius)/little_q
     s = (charge/(4.0*math.pi*constants.epsilon_0))*(2.0/(math.pi*(little_q**(3.0/2.0))*(1.0-mu)))
 
-    radial_electric_field = s*((1.0/mu)*((2.0*ring_radius*special.ellipk(math.sqrt(mu))*(1-mu))- special.ellipe(math.sqrt(mu))*
-                            (2.0*ring_radius-(mu*(radial_distance+ring_radius)))))
+    if(mu != 0):
+        radial_electric_field = s*((1.0/mu)*((2.0*ring_radius*special.ellipk(math.sqrt(mu))*(1-mu))- special.ellipe(math.sqrt(mu))*
+                                (2.0*ring_radius-(mu*(radial_distance+ring_radius)))))
+    else:
+        radial_electric_field = 0
 
     axial_electric_field = s*axial_distance*special.ellipe(math.sqrt(mu))
 
@@ -33,7 +36,12 @@ def e_field_ring_of_charge(charge,radial_distance,axial_distance,ring_radius):
 class TestAll(unittest.TestCase):
 
     def test_e_field_ring_of_charge(self):
-        self.assertAlmostEqual(e_field_ring_of_charge(1.0,0,1e10,1e-10),(constants.Coulomb*1)/((1e10)**2.0), places=3)
+
+        #Compare to point charge at sufficient axial distance
+        self.assertAlmostEqual(e_field_ring_of_charge(1e10,0,1e10,1e-10)[1],((8.987551878368e9)*1e10)/((1e10)**2.0), places=3)
+        #Compare to point charge at sufficient radial distance
+        self.assertAlmostEqual(e_field_ring_of_charge(1e10,1e10,0,1e-10)[0],((8.987551878368e9)*1e10)/((1e10)**2.0), places=3)
+
 
 if __name__ == '__main__':
     unittest.main()
