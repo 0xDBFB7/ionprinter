1a99a4fe4e363d0766a06ca48019ae78482bc749
BasedOnTechnology <therobotist@gmail.com>
2019-04-19 16:53:05 -0400

    works!

--- files/ionprinter/simulation/DFTBA/sfe.py
+++ files/ionprinter/simulation/DFTBA/sfe.py
@@ -52,99 +52,105 @@ def e_field_ring_of_charge(charge,radial_distance,axial_distance,ring_radius):
 X=0
 Y=1
 
-beam_radius = 0.002
-particle_position = [0.0,beam_radius]
-
-particle_mass = 26.0*amu
-initial_energy = 100 #eV
-initial_velocity = math.sqrt((2.0*initial_energy*constants.electron_volt)/(particle_mass))
-
-particle_velocity = [initial_velocity,0.0]
-
-sim_length = 0.01
-
-timestep = (sim_length/1000.0)/initial_velocity
-
-beam_current = 0.01
-
-position_history_x = []
-position_history_y = []
-force_history_y = []
-history_electric_field_x = []
-history_electric_field_y = []
-
-history_rad_distance = []
-history_ax_distance = []
 
 RING_CHARGE = 2
 RING_X = 0
 RING_RADIUS = 1
 
-rings = [[0.005,0.01,1000*constants.e]]#x,radius,charge
-
-for i in range( 0)
-
-while(particle_position[0] < sim_length):
-    particle_position = np.add(particle_position,np.multiply(particle_velocity,timestep))
-
-
-
-    electric_field_x = 0
-    electric_field_y = 0
-    for ring in rings:
-        radial_distance = particle_position[1]
-        axial_distance = particle_position[0]-ring[RING_X]
-        axial_distance = particle_position[0]-ring[RING_X]
-
-        # radial_distance = particle_position[1]
-        radial_electric_field, axial_electric_field = e_field_ring_of_charge(ring[RING_CHARGE],math.fabs(radial_distance),math.fabs(axial_distance),ring[RING_RADIUS])
-        electric_field_x+=axial_electric_field
-        electric_field_y+=radial_electric_field
-
-    electric_field_x /= len(rings)
-    electric_field_y /= len(rings)
-
-    new_scharge_efield = scharge_efield(beam_current,particle_velocity[X],particle_position[Y])*constants.e
-
-    acceleration_y = (electric_field_y*constants.e + new_scharge_efield)/particle_mass
-    acceleration_x = (electric_field_x*constants.e)/particle_mass
-    # particle_velocity[X] += acceleration_x*timestep
-    # particle_velocity[Y] += acceleration_y*timestep
-    print(acceleration_x)
-    print(particle_velocity)
-
-    position_history_x.append(particle_position[0])
-    position_history_y.append(particle_position[1])
-    force_history_y.append(acceleration_y)
-    history_electric_field_x.append(electric_field_x)
-    history_electric_field_y.append(electric_field_y)
-
-    history_rad_distance.append(radial_distance)
-    history_ax_distance.append(axial_distance)
-
-
-
 fig = plt.figure()
 
-plt.subplot(2, 2, 1)
-plt.plot(position_history_x,position_history_y)
-for ring in rings:
-    plt.scatter([ring[RING_X]],[ring[RING_RADIUS]], s=2)
+for c in np.linspace(0,1000000000000*constants.e,10):
 
-plt.subplot(2, 2, 2)
-plt.plot(position_history_x,force_history_y)
 
-plt.subplot(2, 2, 3)
-plt.plot(position_history_x,history_electric_field_x, label='Ex')
-plt.plot(position_history_x,history_electric_field_y, label='Ey')
-plt.legend()
+    beam_radius = 0.005
+    particle_position = [0.0,beam_radius]
 
-plt.subplot(2, 2, 4)
-plt.plot(position_history_x,history_rad_distance, label='Rad')
-plt.plot(position_history_x,history_ax_distance, label='Ax')
-plt.legend()
+    particle_mass = 26.0*amu
+    initial_energy = 100 #eV
+    initial_velocity = math.sqrt((2.0*initial_energy*constants.electron_volt)/(particle_mass))
 
-# plt.scatter(position_history_x,position_history_y)
-# ax = fig.add_subplot(224, projection='3d')
+    particle_velocity = [initial_velocity,0.0]
+
+    sim_length = 0.01
+
+    timestep = (sim_length/500.0)/initial_velocity
+
+    beam_current = 0.01
+
+    position_history_x = []
+    position_history_y = []
+    force_history_y = []
+    history_electric_field_x = []
+    history_electric_field_y = []
+    history_energy = []
+    # history_rad_distance = []
+    # history_ax_distance = []
+
+    rings = []#x,radius,charge
+
+    for x in np.linspace(0.002,0.005,30):
+        rings.append([x,0.01,c])
+
+    while(particle_position[0] < sim_length):
+        particle_position = np.add(particle_position,np.multiply(particle_velocity,timestep))
+
+
+
+        electric_field_x = 0
+        electric_field_y = 0
+        for ring in rings:
+            radial_distance = particle_position[1]
+            axial_distance = particle_position[0]-ring[RING_X]
+
+            # radial_distance = particle_position[1]
+            radial_electric_field, axial_electric_field = e_field_ring_of_charge(ring[RING_CHARGE],math.fabs(radial_distance),math.fabs(axial_distance),ring[RING_RADIUS])
+            electric_field_x+=axial_electric_field
+            electric_field_y+=radial_electric_field
+
+        electric_field_x /= len(rings)
+        electric_field_y /= len(rings)
+
+        new_scharge_efield = scharge_efield(beam_current,particle_velocity[X],particle_position[Y])*constants.e
+
+        acceleration_y = (electric_field_y*constants.e + new_scharge_efield)/particle_mass
+        acceleration_x = (electric_field_x*constants.e)/particle_mass
+        particle_velocity[X] += acceleration_x*timestep
+        particle_velocity[Y] += acceleration_y*timestep
+
+        position_history_x.append(particle_position[0])
+        position_history_y.append(particle_position[1])
+        force_history_y.append(acceleration_y)
+        history_electric_field_x.append(electric_field_x)
+        history_electric_field_y.append(electric_field_y)
+
+        # history_rad_distance.append(radial_distance)
+        # history_ax_distance.append(axial_distance)
+
+        particle_energy = (0.5*particle_mass*(np.linalg.norm(particle_velocity)**2.0))/constants.electron_volt
+        history_energy.append(particle_energy)
+
+    plt.subplot(2, 2, 1)
+    plt.plot(position_history_x,position_history_y)
+    for ring in rings:
+        plt.scatter([ring[RING_X]],[ring[RING_RADIUS]], s=2)
+
+    plt.subplot(2, 2, 2)
+    plt.plot(position_history_x,force_history_y)
+
+    plt.subplot(2, 2, 3)
+    plt.plot(position_history_x,history_electric_field_x, label='Ex')
+    plt.plot(position_history_x,history_electric_field_y, label='Ey')
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    plt.plot(position_history_x,history_energy, label='energy')
+    # plt.plot(position_history_x,history_ax_distance, label='Ax')
+    plt.legend()
+
+    # plt.scatter(position_history_x,position_history_y)
+    # ax = fig.add_subplot(224, projection='3d')
+    # plt.show()
+    plt.pause(0.05)
+    # plt.pause(10)
+
 plt.show()
-plt.pause(10)
