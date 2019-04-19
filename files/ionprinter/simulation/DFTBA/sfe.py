f8030456dd3a1a3813485bee15c9667d36c69519
BasedOnTechnology <therobotist@gmail.com>
2019-04-19 16:25:42 -0400

    Rings seem to be working?

--- files/ionprinter/simulation/DFTBA/sfe.py
+++ files/ionprinter/simulation/DFTBA/sfe.py
@@ -31,6 +31,7 @@ def e_field_ring_of_charge(charge,radial_distance,axial_distance,ring_radius):
     Electric field radial and axial components for the off-axis electric field of
     a infinitesimally thick ring of charge.
     '''
+
     if(axial_distance == 0):
         axial_distance = 1e-10
     little_q = ((radial_distance**2.0) + (ring_radius**2.0) + (axial_distance**2.0) + (2.0*radial_distance*ring_radius))
@@ -38,8 +39,8 @@ def e_field_ring_of_charge(charge,radial_distance,axial_distance,ring_radius):
     s = (charge/(4.0*math.pi*constants.epsilon_0))*(2.0/(math.pi*(little_q**(3.0/2.0))*(1.0-mu)))
 
     if(mu != 0): #ignore the singularity at radial_distance=0
-        radial_electric_field = s*((1.0/mu)*((2.0*ring_radius*special.ellipk(math.sqrt(mu))*(1-mu))- special.ellipe(math.sqrt(mu))*
-                                (2.0*ring_radius-(mu*(radial_distance+ring_radius)))))
+        radial_electric_field = s*((1.0/mu)*((2.0*ring_radius*special.ellipk(math.sqrt(mu))*(1.0-mu))-special.ellipe(math.sqrt(mu))*
+                                ((2.0*ring_radius)-(mu*(radial_distance+ring_radius)))))
     else:
         radial_electric_field = 0
 
@@ -72,10 +73,16 @@ force_history_y = []
 history_electric_field_x = []
 history_electric_field_y = []
 
+history_rad_distance = []
+history_ax_distance = []
+
 RING_CHARGE = 2
 RING_X = 0
 RING_RADIUS = 1
-rings = [[0.005,0.1,10000*constants.e]]#x,radius,charge
+
+rings = [[0.005,0.01,1000*constants.e]]#x,radius,charge
+
+for i in range( 0)
 
 while(particle_position[0] < sim_length):
     particle_position = np.add(particle_position,np.multiply(particle_velocity,timestep))
@@ -86,45 +93,58 @@ while(particle_position[0] < sim_length):
     electric_field_y = 0
     for ring in rings:
         radial_distance = particle_position[1]
-        axial_distance = math.fabs(particle_position[0]-ring[RING_X])
+        axial_distance = particle_position[0]-ring[RING_X]
+        axial_distance = particle_position[0]-ring[RING_X]
+
         # radial_distance = particle_position[1]
-        radial_electric_field, axial_electric_field = e_field_ring_of_charge(ring[RING_CHARGE],radial_distance,axial_distance,ring[RING_RADIUS])
+        radial_electric_field, axial_electric_field = e_field_ring_of_charge(ring[RING_CHARGE],math.fabs(radial_distance),math.fabs(axial_distance),ring[RING_RADIUS])
         electric_field_x+=axial_electric_field
         electric_field_y+=radial_electric_field
 
     electric_field_x /= len(rings)
     electric_field_y /= len(rings)
 
-    scharge_radial_acceleration = (scharge_efield(beam_current,particle_velocity[X],particle_position[Y])*constants.e)/particle_mass
-    particle_velocity[Y] += scharge_radial_acceleration*timestep
-    print(radial_acceleration)
+    new_scharge_efield = scharge_efield(beam_current,particle_velocity[X],particle_position[Y])*constants.e
+
+    acceleration_y = (electric_field_y*constants.e + new_scharge_efield)/particle_mass
+    acceleration_x = (electric_field_x*constants.e)/particle_mass
+    # particle_velocity[X] += acceleration_x*timestep
+    # particle_velocity[Y] += acceleration_y*timestep
+    print(acceleration_x)
     print(particle_velocity)
 
     position_history_x.append(particle_position[0])
     position_history_y.append(particle_position[1])
-    force_history_y.append(radial_acceleration)
+    force_history_y.append(acceleration_y)
     history_electric_field_x.append(electric_field_x)
     history_electric_field_y.append(electric_field_y)
+
+    history_rad_distance.append(radial_distance)
+    history_ax_distance.append(axial_distance)
 
 
 
 fig = plt.figure()
+
 plt.subplot(2, 2, 1)
 plt.plot(position_history_x,position_history_y)
-# plt.scatter(position_history_x,position_history_y, s=2.0)
-# for i in range(0,len(position_history_x)):
-#     plt.plot((position_history_x[i],position_history_x[i]),(0,position_history_y[i]))
-
 for ring in rings:
-    plt.scatter([ring[RING_X]],[ring[RING_RADIUS]])
+    plt.scatter([ring[RING_X]],[ring[RING_RADIUS]], s=2)
 
 plt.subplot(2, 2, 2)
 plt.plot(position_history_x,force_history_y)
+
 plt.subplot(2, 2, 3)
-plt.plot(position_history_x,history_electric_field_x)
-plt.plot(position_history_x,history_electric_field_y)
+plt.plot(position_history_x,history_electric_field_x, label='Ex')
+plt.plot(position_history_x,history_electric_field_y, label='Ey')
+plt.legend()
+
+plt.subplot(2, 2, 4)
+plt.plot(position_history_x,history_rad_distance, label='Rad')
+plt.plot(position_history_x,history_ax_distance, label='Ax')
+plt.legend()
 
 # plt.scatter(position_history_x,position_history_y)
-ax = fig.add_subplot(224, projection='3d')
+# ax = fig.add_subplot(224, projection='3d')
 plt.show()
 plt.pause(10)
