f8020c7a6cd027fc1816d0672a7c6302418a3a37
BasedOnTechnology <therobotist@gmail.com>
2019-04-21 11:32:49 -0400

    Something's gone wrong, beam's going straight up

--- files/ionprinter/simulation/DFTBA/sfe.py
+++ files/ionprinter/simulation/DFTBA/sfe.py
@@ -31,27 +31,14 @@ def scharge_efield(beam_current,beam_velocity,beam_radius,sample_radius=None):
         sample_radius=beam_radius
     return ((beam_current/(2.0*(math.pi)*constants.epsilon_0*beam_velocity)) * (sample_radius/(beam_radius**2.0)))
 
-
-
-
-
 X=0
 Y=1
 
-
-RING_CHARGE = 2
-RING_X = 0
-RING_RADIUS = 1
-
-
 beam_radius = 0.002
-particle_position = [0.0,beam_radius]
 
 particle_mass = 26.0*amu
-initial_energy = 100 #eV
+initial_energy = 1000 #eV
 initial_velocity = math.sqrt((2.0*initial_energy*constants.electron_volt)/(particle_mass))
-
-particle_velocity = [initial_velocity,0.0]
 
 beam_sim_length = 0.01
 
@@ -61,30 +48,14 @@ e_field_sim_length = 0.005
 mesh_x = 50
 mesh_y = 50
 
-potentials = np.zeros(shape=(mesh_x,mesh_y))
-BC = np.zeros(shape=(mesh_x,mesh_y), dtype=bool)
-
-potentials[10,10] = 1000.0
-# potentials[20,10] = -60000.0
-
 mesh_scale_x = mesh_x/e_field_sim_length
 mesh_scale_y = mesh_y/e_field_sim_height
 
-BC[10,10] = True
-
-BC[:,0] = BC[:,-1] = BC[0,:] = BC[-1,:] = True #thanks uhoh! https://physics.stackexchange.com/a/363868/101785
-
 timestep = (beam_sim_length/500.0)/initial_velocity
 
-beam_current = 0.01
+beam_current = 0.001
 
-position_history_x = []
-position_history_y = []
-force_history_y = []
-history_electric_field_x = []
-history_electric_field_y = []
-history_energy = []
-
+beam_count = 5
 # def box(x1,y1,x2,y2):
 #     for x in range(1,mesh_x-1):
 #         for y in range(1,mesh_x-1):
@@ -93,89 +64,140 @@ history_energy = []
 
 start = time.time()
 
-BC_inv = np.invert(BC)
-kernel = 0.25*np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=float)
-convergence = 0
-iterations = 0
-while(True):
-    potentials[BC_inv] = convolve(potentials, kernel)[BC_inv]
-    new_convergence = np.sum(np.absolute(potentials))
-    if(math.fabs(convergence-new_convergence) < 1.0):
-        break
-    convergence = new_convergence
-    iterations += 1
-print(iterations)
+def create_boundary(potentials):
+    '''
+    Takes a (mostly zero) array with the desired geometry potentials
+    '''
+    BC = np.zeros_like(potentials, dtype=bool)
+    BC[potentials.nonzero()] = True #build a boundary condition array from initial Dirchlet conditions
 
-for beam_index in range(0,5):
+    #edges wreak all sorts of havoc
+    return BC
 
-    while(particle_position[0] < beam_sim_length):
+def jacobi_relax_laplace(potentials,BC,tolerance):
+    '''
+    Computes electric field from points using a simple Laplace relaxation.
+    '''
+    BC[:,0] = BC[:,-1] = BC[0,:] = BC[-1,:] = True #thanks uhoh! https://physics.stackexchange.com/a/363868/101785
+    BC_inv = np.invert(BC) #get free grid points
 
-        particle_position = np.add(particle_position,np.multiply(particle_velocity,timestep))
-
-        if((particle_position[X]*mesh_scale_x)+1 < mesh_x and (particle_position[Y]*mesh_scale_y)+1 < mesh_y):
-
-            sub_array = potentials[int(particle_position[X]*mesh_scale_x):int(particle_position[X]*mesh_scale_x)+2,
-                                    int(particle_position[Y]*mesh_scale_y):int(particle_position[Y]*mesh_scale_y)+2]
-            print(sub_array)
-            print(int(particle_position[X]*mesh_scale_x))
-            print(int(particle_position[Y]*mesh_scale_y))
-            grad = np.gradient(sub_array)[0]
-            print(grad)
-            electric_field_x = grad[0][0]*mesh_scale_x #convert to volts/meter
-            electric_field_y = grad[1][0]*mesh_scale_y
-            print(electric_field_x)
-
-            if(BC[int(particle_position[X]*mesh_scale_x),0:int(particle_position[Y]*mesh_scale_y)].any()):
-                print("collision")
-        else:
-            electric_field_x = 0
-            electric_field_y = 0
-
-        new_scharge_efield = scharge_efield(beam_current,particle_velocity[X],particle_position[Y])*constants.e
-
-        acceleration_y = (electric_field_y*constants.e + new_scharge_efield)/particle_mass
-        acceleration_x = (electric_field_x*constants.e)/particle_mass
-        particle_velocity[X] += acceleration_x*timestep
-        particle_velocity[Y] += acceleration_y*timestep
+    kernel = 0.25*np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]], dtype=float) #we want convolve to add adjacent potentials
+    convergence = 0
+    iterations = 0
+    while(True):
+        potentials[BC_inv] = convolve(potentials, kernel)[BC_inv]
+        new_convergence = np.sum(np.absolute(potentials)) #
+        if(math.fabs(convergence-new_convergence) < tolerance):
+            break
+        convergence = new_convergence
+        iterations += 1
+    return potentials
 
 
-        position_history_x.append(particle_position[0])
-        position_history_y.append(particle_position[1])
-        force_history_y.append(new_scharge_efield/constants.e)
-        history_electric_field_x.append(electric_field_x)
-        history_electric_field_y.append(electric_field_y)
 
-        # history_rad_distance.append(radial_distance)
-        # history_ax_distance.append(axial_distance)
+potentials = np.zeros(shape=(mesh_x,mesh_y))
+potentials[10,10] = 1000.0
 
-        particle_energy = (0.5*particle_mass*(np.linalg.norm(particle_velocity)**2.0))/constants.electron_volt
-        history_energy.append(particle_energy)
+BC = create_boundary(potentials)
+potentials = jacobi_relax_laplace(potentials,BC.copy(),1.0)
 
-    stop = time.time()
-    print(stop-start)
+diagnostic_count = 7
+#
+diagnostics = [([[]]*diagnostic_count)]*beam_count #beam count, variable
+# #variable can be
+X_POSITIONS = 0
+Y_POSITIONS = 1
+ENERGY = 2
+SPACE_CHARGE = 3
+E_X = 4
+E_Y = 5
+COLLISION = 6
 
-    plt.subplot(4, 4, 1)
-    plt.title('Beam envelope')
-    plt.plot(position_history_x,position_history_y)
-    plt.ylim((0,0.005))
-    plt.subplot(4, 4, 2)
-    plt.title('Electric field')
-    plt.plot(position_history_x,history_electric_field_x, label='Ex')
-    plt.plot(position_history_x,history_electric_field_y, label='Ey')
-    plt.plot(position_history_x,force_history_y, label='Scharge')
-    plt.legend()
+# for beam_index, current_beam_radius in enumerate(np.linspace(beam_radius,beam_radius/beam_count,beam_count)):
+current_beam_radius = beam_radius
+beam_index = 0
+beam_iteration = 0
+particle_position = [0.0,current_beam_radius]
+print(timestep)
+particle_velocity = [initial_velocity,0.0]
 
-    plt.subplot(4, 4, 3)
-    plt.plot(position_history_x,history_energy, label='energy')
-    # plt.plot(position_history_x,history_ax_distance, label='Ax')
-    plt.legend()
-    plt.subplot(4, 4, 5)
-    plt.imshow(potentials,origin='lower')
+while(particle_position[0] < beam_sim_length):
+
+    particle_position = np.add(particle_position,np.multiply(particle_velocity,timestep))
+
+    diagnostics[beam_index][X_POSITIONS].append(particle_position[0])
+    diagnostics[beam_index][Y_POSITIONS].append(particle_position[1])
+
+    diagnostics[beam_index][COLLISION].append(False)
+
+    if((particle_position[X]*mesh_scale_x)+1 < mesh_x and (particle_position[Y]*mesh_scale_y)+1 < mesh_y):#make sure that we're within the e-field region
+
+        #extract a 2x2 array around the particle
+        sub_array = potentials[int(particle_position[X]*mesh_scale_x):int(particle_position[X]*mesh_scale_x)+2,
+                                int(particle_position[Y]*mesh_scale_y):int(particle_position[Y]*mesh_scale_y)+2]
+
+        grad = np.gradient(sub_array)[0]
+        electric_field_x = grad[0][0]*mesh_scale_x #convert to volts/meter
+        electric_field_y = grad[1][0]*mesh_scale_y
+
+        #check between bottom of plot and current beam envelope
+        if(BC[int(particle_position[X]*mesh_scale_x),0:int(particle_position[Y]*mesh_scale_y)].any()):
+            diagnostics[beam_index][COLLISION][diagnostic_count] = True
+    else:
+        electric_field_x = 0
+        electric_field_y = 0
+
+
+    initial_beam_radius = diagnostics[0][Y_POSITIONS][beam_iteration] #fails if first particle doesn't travel as far? Can't really happen
+    scharge_efield_y = scharge_efield(beam_current,particle_velocity[X],initial_beam_radius,particle_position[Y])
+    acceleration_y = ((electric_field_y + scharge_efield_y)*constants.e)/particle_mass
+    acceleration_x = (electric_field_x*constants.e)/particle_mass
+
+    acceleration_y = 0
+    acceleration_x = 0
+
+    particle_velocity[X] += acceleration_x*timestep
+    particle_velocity[Y] += acceleration_y*timestep
+
+
+    diagnostics[beam_index][SPACE_CHARGE].append(scharge_efield_y)
+    diagnostics[beam_index][E_X].append(electric_field_x)
+    diagnostics[beam_index][E_Y].append(electric_field_y)
+
+    particle_energy = (0.5*particle_mass*(np.linalg.norm(particle_velocity)**2.0))/constants.electron_volt
+    diagnostics[beam_index][ENERGY].append(particle_energy)
+
+
+
+stop = time.time()
+print(stop-start)
+
+labels = (beam_index == 0)
+plt.subplot(4, 4, 1)
+plt.title('Beam envelope')
+plt.plot(diagnostics[beam_index][X_POSITIONS],diagnostics[beam_index][Y_POSITIONS])
+# plt.scatter(diagnostic_collision,[0.0025]*len(diagnostic_collision))
+
+plt.ylim((0,0.005))
+plt.subplot(4, 4, 2)
+plt.title('Electric field')
+plt.plot(diagnostics[beam_index][X_POSITIONS],diagnostics[beam_index][E_X], label='Ex' if labels else "")
+plt.plot(diagnostics[beam_index][X_POSITIONS],diagnostics[beam_index][E_Y], label='Ey' if labels else "")
+plt.plot(diagnostics[beam_index][X_POSITIONS],diagnostics[beam_index][SPACE_CHARGE], label='Scharge')
+plt.legend()
+
+plt.subplot(4, 4, 3)
+plt.title('Particle energy')
+plt.plot(diagnostics[beam_index][X_POSITIONS],diagnostics[beam_index][ENERGY], label='energy')
+# plt.plot(position_history_x,history_ax_distance, label='Ax')
+plt.legend()
+plt.subplot(4, 4, 5)
+plt.title('Electrode potential')
+plt.imshow(potentials,origin='lower')
 
     # plt.scatter(position_history_x,position_history_y)
     # ax = fig.add_subplot(224, projection='3d')
     # plt.show()
 plt.pause(0.05)
-# plt.pause(10)
 
 plt.show()
