f85ba5c8a5120d9bcf206c0605c9412f764afe00
BasedOnTechnology <therobotist@gmail.com>
2019-04-26 15:50:07 -0400

    Doesn't work if more grid points are specified

--- files/ionprinter/simulation/DFTBA/constraint/dftba.cpp
+++ files/ionprinter/simulation/DFTBA/constraint/dftba.cpp
@@ -8,7 +8,7 @@ using namespace std::chrono;
 
 #define BEAM_COUNT 5
 
-
+#define EPSILON_0 8.854187e-12
 
 #define E_MESH_X 50
 #define E_MESH_Y 50
@@ -19,17 +19,18 @@ using namespace std::chrono;
 #define X 0
 #define Y 1
 
-// double scharge_efield(float beam_current, float beam_velocity, float beam_radius, float sample_radius){
-//     //Calculate the electric field at the edge of a beam
-//     //Eq. 44 at https://arxiv.org/pdf/1401.3951.pdf
-//     //returns one value: V/m
-//     //Beam velocity is in m/s
-//
-//     if(!sample_radius):
-//         sample_radius=beam_radius;
-//
-//     return ((beam_current/(2.0*(M_PI)*constants.epsilon_0*beam_velocity)) * (sample_radius/(beam_radius**2.0)))
-// }
+double scharge_efield(float beam_current, float beam_velocity, float beam_radius, float sample_radius){
+    //Calculate the electric field at the edge of a beam
+    //Eq. 44 at https://arxiv.org/pdf/1401.3951.pdf
+    //returns one value: V/m
+    //Beam velocity is in m/s
+
+    if(!sample_radius){
+        sample_radius=beam_radius;
+    }
+
+    return ((beam_current/(2.0*(M_PI)*EPSILON_0*beam_velocity)) * (sample_radius/pow(beam_radius,2.0)));
+}
 
 float float_array_max(float array[E_MESH_X][E_MESH_Y]){
   float max = 0;
@@ -77,17 +78,11 @@ void relax_laplace_potentials(float potentials[E_MESH_X][E_MESH_Y], bool boundar
 }
 
 float x_electric_field_at_position(float potentials[E_MESH_X][E_MESH_Y],int x,int y){
-  return (((potentials[x][y]-potentials[x-1][y])+(potentials[x+1][y]-potentials[x][y]))/2.0)/MESH_SCALE_X;
-  // # sub_array = potentials[int(particle_position[Y]*mesh_scale_y):int(particle_position[Y]*mesh_scale_y)+2,
-  // #                         int(particle_position[X]*mesh_scale_x):int(particle_position[X]*mesh_scale_x)+2]
-  // #
-  // # grad = np.gradient(sub_array)[0]
-  // # electric_field_x = grad[0][0]*mesh_scale_x #convert to volts/meter
-  // # electric_field_y = grad[1][0]*mesh_scale_y
+  return (((potentials[x][y]-potentials[x-1][y])+(potentials[x+1][y]-potentials[x][y]))/2.0)/MESH_SCALE_X; //determine gradient along x, divide by world scale
 }
 
 float y_electric_field_at_position(float potentials[E_MESH_X][E_MESH_Y],int x,int y){
-  return (((potentials[x][y]-potentials[x][y-1])+(potentials[x][y+1]-potentials[x][y]))/2.0)/MESH_SCALE_Y;
+  return (((potentials[x][y]-potentials[x][y-1])+(potentials[x][y+1]-potentials[x][y]))/2.0)/MESH_SCALE_Y; //determine gradient along y, divide by world scale
 }
 
 void display_potentials(float potentials[E_MESH_X][E_MESH_Y]){
@@ -96,9 +91,14 @@ void display_potentials(float potentials[E_MESH_X][E_MESH_Y]){
 
   for(int y = 0; y < E_MESH_Y; y++){
     for(int x = 0; x < E_MESH_X; x++){
-      int r = (potentials[x][y]/((max-min)/2.0))*255.0 * (float)(potentials[x][y] > 0);
-      int g = (potentials[x][y]/((max-min)/2.0))*-255.0 * (float)(potentials[x][y] < 0);
+      int r = 0;
+      int g = 0;
       int b = 0;
+      if(max-min != 0){
+        r = (potentials[x][y]/((max-min)/2.0))*255.0 * (float)(potentials[x][y] > 0);
+        g = (potentials[x][y]/((max-min)/2.0))*-255.0 * (float)(potentials[x][y] < 0);
+        b = 0;
+      }
       printf("\x1b[38;2;%i;%i;%im# ",r,g,b);
     }
     printf("\n");
@@ -109,12 +109,14 @@ void clear_screen(){
   printf("\033[2J\033[1;1H");
 }
 
-float gradient_difference(float desired_gradients[2][E_MESH_X][E_MESH_Y], bool desired_gradient_positions[E_MESH_X][E_MESH_Y], float potentials[E_MESH_X][E_MESH_Y]){
+float gradient_difference(float desired_gradients[2][E_MESH_X][E_MESH_Y], bool desired_gradient_positions[2][E_MESH_X][E_MESH_Y], float potentials[E_MESH_X][E_MESH_Y]){
   float sum = 0;
   for(int y = 1; y < E_MESH_Y-1; y++){
     for(int x = 1; x < E_MESH_X-1; x++){
-      if(desired_gradient_positions[x][y]){
+      if(desired_gradient_positions[X][x][y]){
         sum += std::abs(desired_gradients[X][x][y]-x_electric_field_at_position(potentials,x,y));
+      }
+      if(desired_gradient_positions[Y][x][y]){ //perhaps the Y field can be free while the X field is fixed
         sum += std::abs(desired_gradients[Y][x][y]-y_electric_field_at_position(potentials,x,y));
       }
     }
@@ -125,20 +127,33 @@ float gradient_difference(float desired_gradients[2][E_MESH_X][E_MESH_Y], bool d
 
 
 int main(){
-  float desired_gradients[2][E_MESH_X][E_MESH_Y] = {};
-  desired_gradients[X][25][25] = 1000;
-  desired_gradients[Y][25][25] = -1000;
+  float desired_gradients[2][E_MESH_X][E_MESH_Y] = {}; // the X and Y components of the electric field at each point
+  bool desired_gradient_positions[2][E_MESH_X][E_MESH_Y] = {}; // where the electric field is specified
+  bool electrode_keepout[E_MESH_X][E_MESH_Y] = {};
 
-  bool desired_gradient_positions[E_MESH_X][E_MESH_Y] = {};
-  desired_gradient_positions[25][25] = 1;
+  desired_gradients[X][25][25] = 100;
+  desired_gradients[Y][25][25] = -100;
+  desired_gradient_positions[X][25][25] = 1;
+  desired_gradient_positions[Y][25][25] = 1;
 
+  desired_gradients[X][40][40] = 100;
+  desired_gradients[Y][40][40] = -100;
+  desired_gradient_positions[X][40][40] = 1;
+  desired_gradient_positions[Y][40][40] = 1;
+  // electrode_keepout = 0
   int lowest_points[E_MESH_X][3] = {}; //x,y,voltage
 
-  for(int x = 0; x < E_MESH_X; x++){
+  for(int x = 0; x < E_MESH_X; x+=2){
     float lowest_diff = 0.0;
 
-    for(int y = 0; y < E_MESH_Y; y++){
-      for(int v = -200; v < 200; v+=50){
+    for(int y = 0; y < E_MESH_Y; y+=2){
+
+      if(desired_gradient_positions[X][x][y] || desired_gradient_positions[Y][x][y] || electrode_keepout[x][y]){ // we can't overwrite the beam, silly
+        continue;
+      }
+
+      for(float v = -200; v < 200; v+=50){
+
         float potentials[E_MESH_X][E_MESH_Y] = {};
         bool boundary_conditions[E_MESH_X][E_MESH_Y] = {};
 
@@ -162,8 +177,9 @@ int main(){
         // printf(lowest_diff)
         // printf("%i%% complete\n",(int) (y/E_MESH_Y)*100);
         clear_screen();
-        printf("%f,%f,%f\n",lowest_diff,x_electric_field_at_position(potentials,25,25),x_electric_field_at_position(potentials,25,25));
-        display_potentials(potentials);
+        printf("\x1b[38;2;255;255;0mDan's Hyper Beam Solver\n");
+        printf("\x1b[38;2;255;255;255mcur: %f, lowest: %f, v: %f x: %i y: %i\n",diff,lowest_diff,v,x,y);
+        // display_potentials(potentials);
       }
     }
     // display_potentials(potentials);
