4fe632f4c9e35dbb967a35e8beba8dee6bf509de
BasedOnTechnology <therobotist@gmail.com>
2019-05-29 17:52:18 -0400

    Catch2 compiles waaay too slowly

--- files/ionprinter/simulation/multiphysics/test/multiphysics_test.cpp
+++ files/ionprinter/simulation/multiphysics/test/multiphysics_test.cpp
@@ -1,15 +1,11 @@
 #include "multiphysics.hpp"
-#include "catch.hpp"
+#include "QUnit.hpp"
 #include <iostream>
-#include <vector>
 
 int main() {
+    QUnit::UnitTest qunit(std::cerr, QUnit::normal);
 
-    std::vector<bool> mesh_present(10000);
-    double bounds[6];
-    int mesh_geometry[3];
-    float mesh_scale[3];
-    float translate[3] = {0};
-    import_mesh("../10x10x10_cube.stl",mesh_present,mesh_geometry,mesh_scale,bounds,translate);
+    QUNIT_IS_EQUAL(42, 41);
 
+    return qunit.errors();
 }
