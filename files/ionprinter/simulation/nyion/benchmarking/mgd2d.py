674c6291575afaece2a0fc2e6bf93ee83afff88b
BasedOnTechnology <therobotist@gmail.com>
2019-08-31 16:45:44 -0400

    Borrowing Dr. Malpeddi's implementation

---
+++ files/ionprinter/simulation/nyion/benchmarking/mgd2d.py
@@ -0,0 +1,131 @@
+"""
+2017 A. R. Malipeddi
+A simple 2D geometric multigrid solver for the homogeneous Dirichlet Poisson problem on Cartesian grids and unit square. Cell centered 5-point finite difference operator.
+
+Thanks Dr. Malpeddi!
+"""
+
+import numpy as np
+from scipy.sparse.linalg import LinearOperator
+
+def Jacrelax(level,nx,ny,u,f,iters=1,pre=False):
+  '''
+  under-relaxed Jacobi method smoothing
+  '''
+
+  dx=1.0/nx; dy=1.0/ny
+  Ax=1.0/dx**2; Ay=1.0/dy**2
+  Ap=1.0/(2.0*(Ax+Ay))
+
+  #Dirichlet BC
+  u[ 0,:] = -u[ 1,:]
+  u[-1,:] = -u[-2,:]
+  u[:, 0] = -u[:, 1]
+  u[:,-1] = -u[:,-2]
+
+  #if it is a pre-sweep, u is fully zero (on the finest grid depends on BC, always true on coarse grids)
+  # we can save some calculation, if doing only one iteration, which is typically the case.
+  if(pre and level>1):
+    u[1:nx+1,1:ny+1] = -Ap*f[1:nx+1,1:ny+1]
+    #Dirichlet BC
+    u[ 0,:] = -u[ 1,:]
+    u[-1,:] = -u[-2,:]
+    u[:, 0] = -u[:, 1]
+    u[:,-1] = -u[:,-2]
+    iters=iters-1
+
+  for it in range(iters):
+    u[1:nx+1,1:ny+1] = Ap*(Ax*(u[2:nx+2,1:ny+1] + u[0:nx,1:ny+1])
+                         + Ay*(u[1:nx+1,2:ny+2] + u[1:nx+1,0:ny])
+                         - f[1:nx+1,1:ny+1])
+    #Dirichlet BC
+    u[ 0,:] = -u[ 1,:]
+    u[-1,:] = -u[-2,:]
+    u[:, 0] = -u[:, 1]
+    u[:,-1] = -u[:,-2]
+
+#  if(not pre):
+#    return u,None
+
+  res=np.zeros([nx+2,ny+2])
+  res[1:nx+1,1:ny+1]=f[1:nx+1,1:ny+1]-(( Ax*(u[2:nx+2,1:ny+1]+u[0:nx,1:ny+1])
+                                       + Ay*(u[1:nx+1,2:ny+2]+u[1:nx+1,0:ny])
+                                       - 2.0*(Ax+Ay)*u[1:nx+1,1:ny+1]))
+  return u,res
+
+def restrict(nx,ny,v):
+  '''
+  restrict 'v' to the coarser grid
+  '''
+  v_c=np.zeros([nx+2,ny+2])
+
+#  #vectorized form of
+#  for i in range(1,nx+1):
+#    for j in range(1,ny+1):
+#      v_c[i,j]=0.25*(v[2*i-1,2*j-1]+v[2*i,2*j-1]+v[2*i-1,2*j]+v[2*i,2*j])
+
+  v_c[1:nx+1,1:ny+1]=0.25*(v[1:2*nx:2,1:2*ny:2]+v[1:2*nx:2,2:2*ny+1:2]+v[2:2*nx+1:2,1:2*ny:2]+v[2:2*nx+1:2,2:2*ny+1:2])
+
+  return v_c
+
+def prolong(nx,ny,v):
+  '''
+  interpolate 'v' to the fine grid
+  '''
+  v_f=np.zeros([2*nx+2,2*ny+2])
+
+#  #vectorized form of
+  for i in range(1,nx+1):
+      for j in range(1,ny+1):
+        v_f[2*i-1,2*j-1] = 0.5625*v[i,j]+0.1875*(v[i-1,j]+v[i,j-1])+0.0625*v[i-1,j-1]
+        v_f[2*i  ,2*j-1] = 0.5625*v[i,j]+0.1875*(v[i+1,j]+v[i,j-1])+0.0625*v[i+1,j-1]
+        v_f[2*i-1,2*j  ] = 0.5625*v[i,j]+0.1875*(v[i-1,j]+v[i,j+1])+0.0625*v[i-1,j+1]
+        v_f[2*i  ,2*j  ] = 0.5625*v[i,j]+0.1875*(v[i+1,j]+v[i,j+1])+0.0625*v[i+1,j+1]
+
+  return v_f
+
+def V_cycle(nx,ny,num_levels,u,f,level=1):
+  '''
+  V cycle
+  '''
+  if(level==num_levels):#bottom solve
+    u,res=Jacrelax(level,nx,ny,u,f,iters=50,pre=True)
+    return u,res
+
+  #Step 1: Relax Au=f on this grid
+  u,res=Jacrelax(level,nx,ny,u,f,iters=2,pre=True)
+
+  #Step 2: Restrict residual to coarse grid
+  res_c=restrict(nx//2,ny//2,res)
+
+  #Step 3:Solve A e_c=res_c on the coarse grid. (Recursively)
+  e_c=np.zeros_like(res_c)
+  e_c,res_c=V_cycle(nx//2,ny//2,num_levels,e_c,res_c,level+1)
+
+  #Step 4: Interpolate(prolong) e_c to fine grid and add to u
+  u+=prolong(nx//2,ny//2,e_c)
+
+  #Step 5: Relax Au=f on this grid
+  u,res=Jacrelax(level,nx,ny,u,f,iters=1,pre=False)
+  return u,res
+
+def FMG(nx,ny,num_levels,f,nv=1,level=1):
+
+  if(level==num_levels):#bottom solve
+    u=np.zeros([nx+2,ny+2])
+    u,res=Jacrelax(level,nx,ny,u,f,iters=50,pre=True)
+    return u,res
+
+  #Step 1: Restrict the rhs to a coarse grid
+  f_c=restrict(nx//2,ny//2,f)
+
+  #Step 2: Solve the coarse grid problem using FMG
+  u_c,_=FMG(nx//2,ny//2,num_levels,f_c,nv,level+1)
+
+  #Step 3: Interpolate u_c to the fine grid
+  u=prolong(nx//2,ny//2,u_c)
+
+  #step 4: Execute 'nv' V-cycles
+  for _ in range(nv):
+    u,res=V_cycle(nx,ny,num_levels-level,u,f)
+  return u,res
---
+++ files/ionprinter/simulation/nyion/benchmarking/mgtest.py
@@ -0,0 +1,51 @@
+"""
+This is an example showing how to call the mgd2d FMG solver.
+"""
+import numpy as np
+import time
+from mgd2d import FMG
+
+def Uann(x,y,n):#analytical solution
+   return (x**3-x)*(y**3-y)
+
+def source(x,y,n):#RHS corresponding to above
+  return 6*x*y*(x**2+ y**2 - 2)
+
+#input
+#FMG is a direct solver. tolerance and iterations are not used
+nv          = 1     # nv : Number of V-cycles within FMG. nv=1 will give solution a to within discretization error.
+                    # Increase this (to ~5) to get exact solution (upto roundoff limit) of the discrete problem
+                    # For instance, when deferred correction is desired.
+nlevels     = 8     # total number of grid levels. Also determines the grid size
+NX          = 1*2**(nlevels-1)
+NY          = 1*2**(nlevels-1)
+
+#the grid has one layer of ghost cells to help apply the boundary conditions
+uann=np.zeros([NX+2,NY+2])#analytical solution
+f   =np.zeros([NX+2,NY+2])#RHS
+
+#calcualte the RHS and exact solution
+DX=1.0/NX
+DY=1.0/NY
+
+xc=np.linspace(0.5*DX,1-0.5*DX,NX)
+yc=np.linspace(0.5*DY,1-0.5*DY,NY)
+XX,YY=np.meshgrid(xc,yc,indexing='ij')
+
+uann[1:NX+1,1:NY+1]=Uann(XX,YY,1)
+f[1:NX+1,1:NY+1]   =source(XX,YY,1)
+
+print('mgd2d.py FMG solver:')
+print('NX:',NX,', NY:',NY,', levels: ',nlevels)
+
+# solving
+tb=time.time()
+u,res=FMG(NX,NY,nlevels,f,nv)
+tf=time.time()
+
+rtol=np.max(np.max(np.abs(res)))
+print(' FMG L_inf(res.)= ',rtol)
+
+print('Elapsed time: ',tf-tb,' seconds')
+error=uann[1:NX+1,1:NY+1]-u[1:NX+1,1:NY+1]
+print('L_inf (true error): ',np.max(np.max(np.abs(error))))
