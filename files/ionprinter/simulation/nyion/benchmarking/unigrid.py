49921a723ea72a22fa9460873971944567c9fd4d
BasedOnTechnology <therobotist@gmail.com>
2019-09-02 23:45:45 -0400

    Gosh darnit, a sneaky exponential function!

--- files/ionprinter/simulation/nyion/benchmarking/unigrid.py
+++ files/ionprinter/simulation/nyion/benchmarking/unigrid.py
@@ -23,36 +23,44 @@ b = numpy.zeros(SIZE_X)
 def FND(I3, J3):
     return (M1-math.fabs(I-I3))*(M1-math.fabs(J-J3))/(M1*M1)
 
-I1 = 97
-J1 = 65
+I1 = 63
+J1 = 63
 H = 1/8##?
 
 U = numpy.zeros((I1,J1))
 F = numpy.zeros((I1,J1))
+B = numpy.zeros((I1,J1))
+for I in range(0,I1):
+    for J in range(0,J1):
+#         U[I,J] = 10
+#         U[I+15,J] = -10
+# #
+#         B[I,J] = 1
+#         B[I+15,J] = 1
+         U[I,J] = math.cos(3.0*(I+J-2)*H)
 
-# for I in range(0,I1):
-U[30,30] = 100
 
-#     for J in range(0,J1):
-#         U[I,J] = math.cos(3.0*(I+J-2)*H)
-# U[10,10] = 10
-
-N=6
+N=4
 prev_E = 1
 while True:
-    for k in range(1,N+1): #levels
+
+    #
+    for k in range(0,N+1): #levels
         M1 = 2**(N-k)
+        print(M1)
         for relaxes in range(0,1): #Number of relaxations; 1 usually suffices
             E=0
-            for I in range(1+M1,I1-M1,M1):
-                for J in range(1+M1,J1-M1,M1):
+            T = U.copy()
+            for I in range(M1,I1-M1,M1):
+                for J in range(M1,J1-M1,M1):
                     A1=0
                     R1=0
                     for I3 in range(I-M1+1,I+M1): #fix ranges
-                        for J3 in range(J-M1+1,J+M1):
-                            D = 4 + math.exp((I3+J3)*H)*H*H
-                            F[I3,J3] = 0
+                         for J3 in range(J-M1+1,J+M1):
+                            D = 4
+                            F[I3,J3] = math.sin(3.0*(I3+J3-2)*H)*H*H
                             R = (D*U[I3,J3]) - U[I3,J3-1] - U[I3,J3+1] - U[I3-1,J3] - U[I3+1,J3] - F[I3,J3] #compute residual
+                            # R *= (1.0-B[I3,J3])
                             A3 = D*FND(I3,J3) - FND(I3,J3+1) - FND(I3,J3-1) - FND(I3+1,J3) - FND(I3-1,J3)
                             R1 = R1 + FND(I3,J3)*R
                             A1 = A1 + FND(I3,J3)*A3
@@ -60,7 +68,16 @@ while True:
                     E=E+R1*R1
                     for I3 in range(I-M1+1,I+M1):
                         for J3 in range(J-M1+1,J+M1):
-                            U[I3,J3] = U[I3,J3] - S*FND(I3,J3)
+                            # if(not B[I3,J3]):
+                                U[I3,J3] = U[I3,J3] - S*FND(I3,J3)
+            # numpy.copyto(U,T)
+            # for I in range(40,50):
+            #     for J in range(25,35):
+            #         U[I,J] = 10
+            #         U[I+15,J] = -10
+            #
+            #         B[I,J] = 1
+            #         B[I+15,J] = 1
 
         E=math.sqrt(E)/M1/H
         print(E)
