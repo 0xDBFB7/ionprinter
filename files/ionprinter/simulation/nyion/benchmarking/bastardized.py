66078262264bb734a87f1849645510fd4faad5c1
BasedOnTechnology <therobotist@gmail.com>
2019-10-04 22:27:56 -0400

    Higher-order stencils work great

--- files/ionprinter/simulation/nyion/benchmarking/bastardized.py
+++ files/ionprinter/simulation/nyion/benchmarking/bastardized.py
@@ -10,12 +10,14 @@ SIZE_Z = 32
 
 u = numpy.zeros((SIZE_X, SIZE_Y, SIZE_Z))
 b = numpy.zeros((SIZE_X, SIZE_Y, SIZE_Z))
+f = numpy.zeros((SIZE_X, SIZE_Y, SIZE_Z))
 
 for x in range(10,20):
     for y in range(10,20):
+        u[x,y,8] = 100
         b[x,y,8] = 1
 
-def gauss_seidel(U,b,theta):
+def gauss_seidel(U,B,F,theta):
     rows = U.shape[0]
     cols = U.shape[1]
     z_size = U.shape[2]
@@ -23,16 +25,17 @@ def gauss_seidel(U,b,theta):
     for x in range(theta, (rows - theta)-1,theta):
         for y in range(theta, (cols - theta)-1,theta):
             for z in range(theta, (z_size - theta)-1,theta):
-                U[x,y,z] = (U[x+theta,y,z] +
-                            U[x-theta,y,z] +
-                            U[x,y+theta,z] +
-                            U[x,y-theta,z] +
-                            U[x,y,z+theta] +
-                            U[x,y,z-theta] + b[x,y,z])/6.0
+                if(not B[x,y,z]):
+                    U[x,y,z] = (U[x+theta,y,z] +
+                                U[x-theta,y,z] +
+                                U[x,y+theta,z] +
+                                U[x,y-theta,z] +
+                                U[x,y,z+theta] +
+                                U[x,y,z-theta] + F[x,y,z])/6.0
 
 
 
-def jacobi(U,T,b,theta):
+def jacobi(U,T,B,F,theta):
     rows = U.shape[0]
     cols = U.shape[1]
     z_size = U.shape[2]
@@ -40,12 +43,15 @@ def jacobi(U,T,b,theta):
     for x in range(theta, (rows - theta)-1,theta):
         for y in range(theta, (cols - theta)-1,theta):
             for z in range(theta, (z_size - theta)-1,theta):
-                T[x,y,z] = (U[x+theta,y,z] +
-                            U[x-theta,y,z] +
-                            U[x,y+theta,z] +
-                            U[x,y-theta,z] +
-                            U[x,y,z+theta] +
-                            U[x,y,z-theta] + b[x,y,z])/6.0
+                if(not B[x,y,z]):
+                    T[x,y,z] = (U[x+theta,y,z] +
+                                U[x-theta,y,z] +
+                                U[x,y+theta,z] +
+                                U[x,y-theta,z] +
+                                U[x,y,z+theta] +
+                                U[x,y,z-theta] +
+                                U[x,y-theta,z-theta] +
+                                U[x,y+theta,z+theta] +  F[x,y,z])/8.0
 
 
 def restriction(X, theta):
@@ -61,8 +67,20 @@ def restriction(X, theta):
                     for j in range(0,theta):
                         for k in range(0,theta):
                             sum += X[x+i,y+j,z+k]
-                X[x,y,z] = sum
+                if(not b[x,y,z]):
+                    X[x,y,z] = sum
 
+
+# def residual(U,R,F):
+#     rows = U.shape[0]
+#     cols = U.shape[1]
+#     z_size = U.shape[2]
+#     theta = 1
+#     for x in range(theta, (rows - theta),theta):
+#         for y in range(theta, (cols - theta),theta):
+#             for z in range(theta, (z_size - theta),theta):
+#                 if(not b[x,y,z]):
+#                     R[x,y,z] = F[x,y,z] + (U[x+1,y,z] + U[x-1,y,z] + U[x,y+1,z] + U[x,y-1,z] + U[x,y,z+1] + U[x,y,z-1] - 6.0*U[x,y,z])
 
 def prolongate(X, theta):
     rows = X.shape[0]
@@ -82,18 +100,19 @@ def prolongate(X, theta):
                 for i in range(0,theta):
                     for j in range(0,theta):
                         for k in range(0,theta):
-                            f_x = float(i)/theta
-                            f_y = float(j)/theta
-                            f_z = float(k)/theta
+                            if(not b[x+i,y+j,z+k]):
+                                f_x = float(i)/theta
+                                f_y = float(j)/theta
+                                f_z = float(k)/theta
 
-                            X[x+i,y+j,z+k] = 0
-                            X[x+i,y+j,z+k] += V000*(1.0-f_x)*(1.0-f_y)*(1.0-f_z)
-                            X[x+i,y+j,z+k] += V001*(1.0-f_x)*(1.0-f_y)*(f_z)
-                            X[x+i,y+j,z+k] += V010*(1.0-f_x)*(f_y)*(1.0-f_z)
-                            X[x+i,y+j,z+k] += V100*(f_x)*(1.0-f_y)*(1.0-f_z)
-                            X[x+i,y+j,z+k] += V101*(f_x)*(1.0-f_y)*(f_z)
-                            X[x+i,y+j,z+k] += V110*(f_x)*(f_y)*(1.0-f_z)
-                            X[x+i,y+j,z+k] += V111*(f_x)*(f_y)*(f_z)
+                                X[x+i,y+j,z+k] = 0
+                                X[x+i,y+j,z+k] += V000*(1.0-f_x)*(1.0-f_y)*(1.0-f_z)
+                                X[x+i,y+j,z+k] += V001*(1.0-f_x)*(1.0-f_y)*(f_z)
+                                X[x+i,y+j,z+k] += V010*(1.0-f_x)*(f_y)*(1.0-f_z)
+                                X[x+i,y+j,z+k] += V100*(f_x)*(1.0-f_y)*(1.0-f_z)
+                                X[x+i,y+j,z+k] += V101*(f_x)*(1.0-f_y)*(f_z)
+                                X[x+i,y+j,z+k] += V110*(f_x)*(f_y)*(1.0-f_z)
+                                X[x+i,y+j,z+k] += V111*(f_x)*(f_y)*(f_z)
 
 # Precondition.
 # for i in range(0,10):
@@ -104,12 +123,19 @@ ims = []
 t = 0
 c1 = 1
 while True:
-
+    for i in range(0,10):
+        T1 = numpy.zeros((SIZE_X, SIZE_Y, SIZE_Z))
+        jacobi(u,T1,b,f,1)
+        u = T1.copy()
     # Step 1: Residual Calculation.
     v1 = u.copy()
-    gauss_seidel(u,b,1)
+    T1 = numpy.zeros((SIZE_X, SIZE_Y, SIZE_Z))
+    jacobi(u,T1,b,f,1)
+    u = T1.copy()
     # u=T.copy()
     r = u - v1
+    # r = numpy.zeros((SIZE_X, SIZE_Y, SIZE_Z))
+    # residual(u,r,f)
     # Step 2: Restriction.
     res = [32,16,8,4,2,1]
     v=0
@@ -129,7 +155,7 @@ while True:
         # plt.gca().set_title('Restricted')
         # plt.imshow(r1[:,:,8])
         for i in range(0,3*int(math.sqrt(res[level]))):
-            jacobi(v,T,r1,2*resolution)
+            jacobi(v,T,numpy.zeros_like(v),r1,2*resolution)
             v=T.copy()
         # plt.subplot(4, 1, 3)
         # plt.gca().set_title('Smoothed')
@@ -147,7 +173,7 @@ while True:
         # plt.savefig(str(t) + '.png')
 
     #
-    u = u + v
+    u = u + (v * (1.0-b))
     convergence.append(numpy.linalg.norm(r))
     #
     plt.subplot(2, 2, 1)
