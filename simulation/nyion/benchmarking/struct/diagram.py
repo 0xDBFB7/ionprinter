c3d0937753f2b0ae56aad1ecb59c753dda21b991
BasedOnTechnology <therobotist@gmail.com>
2019-11-07 00:02:45 -0500

    test

--- simulation/nyion/benchmarking/struct/diagram.py
+++ simulation/nyion/benchmarking/struct/diagram.py
@@ -5,26 +5,36 @@ g = Digraph(comment='The Round Table')
 
 
 
-def new_block(scale,origin,address,length):
+def new_block(scale,origin,address,length,label):
     with g.subgraph(name='cluster_'+str(address), node_attr={'shape': 'record'}) as a:
-        #a.attr(rank = 'same')
-        for i in range(length-1,-1,-1):
+        
+        a.attr(label = label)
+        for i in range(0,length):
             a.node(str(address+i),
-                label='<{{X={} | W<sub>x</sub> = {} | Buf {} | Ref 0}}>'
+                label='<{{X={0} | W<sub>x</sub> = {1:.4f} | Buf {2} | Ref 0}}>'
                 .format(i,((i-1)*scale+origin)*(not(i == 0 or i == length-1)),address+i),    
                 style="filled", fillcolor=('green' if i==0 or i==length-1 else 'white'))        
 
-        a.body.append('{rank=same;' + ';'.join([str(address+i) for i in range(0,length)]) + '}')      
-        a.body.append(' -> '.join([str(address+i) for i in range(0,length)]) + '[style=invis]')
+        a.body.append('{rank=same;' + ';'.join([str(address+i) for i in range(0,length)]) + '}')
+        a.body.append(' -> '.join([str(address+i) for i in range(0,length)]) + '[style=invis,weight=4.0]')
 
 
-new_block(0.01,0.0,0,8)
+new_block(0.01,0.0,0,8,'')
 
-new_block(0.01,0.0,8,8)
+new_block(0.01/6,0.0,8,8,'')
 
-g.edge('1','9',color='red',minlen='20',penwidth='3')
-g.edge('2','14',color='red',minlen='20',penwidth='3')
-    
+new_block(0.01/6,0.0083,16,8,'')
+
+g.edge('1','9',color='red',minlen='5',penwidth='3')
+g.edge('2','14',color='red',minlen='5',penwidth='3')
+
+g.edge('14','16',color='blue',minlen='2',penwidth='3')
+g.edge('17','15',color='blue',minlen='2',penwidth='3')
+
+g.edge('2','17',color='red',minlen='2',penwidth='3')
+g.edge('3','22',color='red',minlen='2',penwidth='3')
+
+
 print(g.source)  
 g.format = 'svg'
 g.render()
