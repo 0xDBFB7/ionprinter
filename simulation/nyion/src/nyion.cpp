4af2ad806b447d0f616d83213b74d4f6a4b27fe5
BasedOnTechnology <therobotist@gmail.com>
2019-12-25 14:43:21 -0500

    CMake build working

---
+++ simulation/nyion/src/nyion.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TBrowser.h"
+#include "TFrame.h"
+#include "TApplication.h"
+
+
+
+int main(int argc, char **argv)
+{
+  TApplication *myApp = new TApplication("myApp", &argc, argv);
+	TCanvas *c1 = new TCanvas("c1", "c1", 200, 10, 1280, 720);
+	TH1F h1("h1","Histo from a Gaussian",100,-3,3);
+	h1.FillRandom("gaus", 10000);
+	h1.Draw();
+
+	TFile *file = new TFile("../Debug/test.root", "RECREATE");
+	file->WriteTObject(c1);
+
+	c1->Update();
+	c1->GetFrame()->SetBorderSize(12);
+	c1->Modified();
+
+	myApp->Run();
+	return 0;
+}
+
+//
+// int main(int ac, char** av){
+//     TApplication theApp("App", &argc, argv);
+//     TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",200,10,500,300);
+//     Double_t x[100], y[100];
+//     Int_t n = 20;
+//     for (Int_t i=0;i<n;i++) {
+//     x[i] = i*0.1;
+//     y[i] = 10*sin(x[i]+0.2);
+//     }
+//     TGraph* gr = new TGraph(n,x,y);
+//     gr->Draw("AC*");
+//   return RUN_ALL_TESTS(ac, av);
+// }
--- simulation/nyion/test/nyion_test.cpp
+++ simulation/nyion/test/nyion_test.cpp
@@ -1,33 +1,4 @@
-#include "nyion.hpp"
-//#include "visualize.hpp"
-#include "CppUTest/CommandLineTestRunner.h"
-#include "CppUTest/TestHarness.h"
 
-// TEST_GROUP(MG_GPU_OPERATORS)
-// {
-
-
-
-int main(int argc, char **argv)
-{
-  TApplication *myApp = new TApplication("myApp", &argc, argv);
-	TCanvas *c1 = new TCanvas("c1", "c1", 200, 10, 1280, 720);
-	TH1F h1("h1","Histo from a Gaussian",100,-3,3);
-	h1.FillRandom("gaus", 10000);
-	h1.Draw();
-
-	TFile *file = new TFile("../Debug/test.root", "RECREATE");
-	file->WriteTObject(c1);
-
-	c1->Update();
-	c1->GetFrame()->SetBorderSize(12);
-	c1->Modified();
-
-	myApp->Run();
-	return 0;
-}
-
-//
 // int main(int ac, char** av){
 //     TApplication theApp("App", &argc, argv);
 //     TCanvas *c1 = new TCanvas("c1","A Simple Graph Example",200,10,500,300);
